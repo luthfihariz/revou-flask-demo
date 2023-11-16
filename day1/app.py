5ede5137da4b97473ffe9df05a675f19dab0f57a
Luthfi Hariz <luthfihariz@gmail.com>
2023-11-16 12:11:18 +0700

    Day1 Practice

--- day1/app.py
+++ day1/app.py
@@ -14,3 +14,19 @@ def index():
 @app.route("/dummy")
 def dummy():
     return {"error":"Token is not valid!"}, 403 # Response Object
+
+
+book_list = [{'title': 'Dummy Book Title', 'author': 'Luthfi Hariz'}]
+
+@app.route("/books", methods=["GET", "POST"])
+def handle_books():
+    if request.method == "GET":
+        return book_list
+    else:
+        title = request.form.get('title')
+        author = request.form.get('author')
+        book_list.append({
+            'title': title,
+            'author': author
+        })
+        return {}, 201
