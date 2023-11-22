3db4d5b3adba966cc59ccaa7597497dd1e738000
Luthfi Hariz <luthfihariz@gmail.com>
2023-11-22 10:23:08 +0700

    jwt-jwt

--- day3/auth/apis.py
+++ day3/auth/apis.py
@@ -3,6 +3,9 @@ from user.model import User
 from db import db
 from common.bcrypt import bcrypt
 from marshmallow import Schema, fields, ValidationError, validate, validates
+import jwt
+import os
+from datetime import datetime, timedelta
 
 auth_blueprint = Blueprint('auth', __name__)
 
@@ -32,6 +35,42 @@ def register():
     db.session.commit()
 
     return schema.dump(new_user)
+
+
+class UserLoginSchema(Schema):
+    username = fields.String(required=True, validate=validate.Length(min=3))
+    password = fields.String(required=True, validate=validate.Length(min=5), load_only=True)
+
+
+@auth_blueprint.route("/login", methods=["POST"])
+def login():
+    schema = UserLoginSchema()
+    try:
+        data = schema.load(request.get_json())
+    except ValidationError as err:
+        return {"error": err.messages}, 400
+
+    user = User.query.filter_by(username=data['username']).first()
+    if not user:
+        return {"error": "User or password is not valid"}, 401
+    
+    valid = bcrypt.check_password_hash(user.password, data['password'])
+
+    if not valid:
+        return {"error": "User or password is not valid"}, 401
+    
+    token = jwt.encode({
+        'user_id': user.id,
+        'username': user.username,
+        'exp': datetime.utcnow() + timedelta(seconds=10)
+    }, os.getenv('SECRET_KEY'), algorithm='HS256')
+    
+    return {
+        'id': user.id,
+        'username': user.username,
+        'email': user.email,
+        'token': token
+    }
 
 # @auth_blueprint.route("/registration", methods=["POST"])
 # def register():
--- day3/user/apis.py
+++ day3/user/apis.py
@@ -2,11 +2,19 @@ from flask import Blueprint, request
 from user.model import User
 from db import db
 from common.bcrypt import bcrypt
+import jwt
+import os
+from auth.utils import decode_jwt
 
 user_blueprint = Blueprint("user_blueprint", __name__)
 
-@user_blueprint.route("/<int:user_id>", methods=["GET"])
-def get_user(user_id):
+@user_blueprint.route("", methods=["GET"])
+def get_user():
+    payload = decode_jwt(request.headers.get('Authorization'))
+    if not payload:
+        return {'message': 'Token invalid or expired. Please login again'}, 401
+    
+    user_id = payload.get('user_id')    
     user = User.query.get(user_id)
     if not user:
         return {"error": "User not found"}, 404
